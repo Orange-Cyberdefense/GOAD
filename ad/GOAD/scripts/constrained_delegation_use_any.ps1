be265ec1fbc8f149230869dfe60126bbb7c24a89
Mayfly277 <mayfly277@gmail.com>
2024-01-15 14:59:02 +0100

    fix spn override by mistake

--- ad/GOAD/scripts/constrained_delegation_use_any.ps1
+++ ad/GOAD/scripts/constrained_delegation_use_any.ps1
@@ -1,4 +1,3 @@
-# https://www.thehacker.recipes/ad/movement/kerberos/delegations/constrained#with-protocol-transition
-Set-ADUser -Identity "jon.snow" -ServicePrincipalNames @{Add='CIFS/winterfell.north.sevenkingdoms.local'}
+Set-ADUser -Identity "jon.snow" -ServicePrincipalNames @{Add='CIFS/thewall.north.sevenkingdoms.local'}
 Get-ADUser -Identity "jon.snow" | Set-ADAccountControl -TrustedToAuthForDelegation $true
 Set-ADUser -Identity "jon.snow" -Add @{'msDS-AllowedToDelegateTo'=@('CIFS/winterfell.north.sevenkingdoms.local','CIFS/winterfell')}
