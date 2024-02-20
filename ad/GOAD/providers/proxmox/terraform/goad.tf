65e5e10fd149d9a7145825f86b9f3d1526d560e9
Zapfmeister <zapfmeister@gmail.com>
2024-02-20 09:11:41 +0100

    Change terraform provider to bpg/proxmox

--- ad/GOAD/providers/proxmox/terraform/goad.tf
+++ ad/GOAD/providers/proxmox/terraform/goad.tf
@@ -1,139 +1,310 @@
-resource "proxmox_vm_qemu" "dc01" {
-    name = "GOAD-DC01"
-    desc = "DC01 - windows server 2019 - 192.168.10.10"
-    qemu_os = "win10"
-    target_node = var.pm_node
-    pool = var.pm_pool
+resource "proxmox_virtual_environment_vm" "dc01" {
+  name        = "GOAD-DC01"
+  description = "DC01 - windows server 2019 - 192.168.10.10"
+  node_name   = var.pm_node
+  pool_id     = var.pm_pool
 
+  operating_system {
+    type = "win10"
+  }
+
+  cpu {
+    cores   = 2
     sockets = 1
-    cores = 2
-    memory = 3096
-    agent = 1
-    clone = "WinServer2019x64-cloudinit-qcow2"
-    full_clone = var.pm_full_clone
+  }
 
-   network {
-     bridge    = "vmbr3"
-     model     = "e1000"
-     tag       = 10
-   }
-    lifecycle {
-      ignore_changes = [
-        disk,
+  memory {
+    dedicated = 3096
+  }
+
+  clone {
+    vm_id = var.vm_id_winsrv2019
+    full  = var.pm_full_clone
+  }
+  agent {
+    # read 'Qemu guest agent' section, change to true only when ready
+    enabled = true
+  }
+
+  network_device {
+    bridge  = "vmbr3"
+    model   = "e1000"
+    vlan_id = 10
+  }
+
+  initialization {
+    datastore_id = var.storage
+    dns {
+      servers = [
+        "192.168.10.1",
+        "1.1.1.1",
       ]
     }
-   nameserver = "192.168.10.1"
-   ipconfig0 = "ip=192.168.10.10/24,gw=192.168.10.1"
+    ip_config {
+      ipv4 {
+        address = "192.168.10.10/24"
+        gateway = "192.168.10.1"
+      }
+    }
+  }
 }
 
-resource "proxmox_vm_qemu" "dc02" {
-    name = "GOAD-DC02"
-    desc = "DC02 - windows server 2019 - 192.168.10.11"
-    qemu_os = "win10"
-    target_node = var.pm_node
-    pool = var.pm_pool
-    sockets = 1
-    cores = 2
-    memory = 3096
-    agent = 1
-    clone = "WinServer2019x64-cloudinit-qcow2"
-    full_clone = var.pm_full_clone
+resource "proxmox_virtual_environment_vm" "dc02" {
+  name        = "GOAD-DC02"
+  description = "DC02 - windows server 2019 - 192.168.10.11"
+  node_name   = var.pm_node
+  pool_id     = var.pm_pool
 
-    network {
-      bridge    = "vmbr3"
-      model     = "e1000"
-      tag       = 10
-    }
-    lifecycle {
-      ignore_changes = [
-        disk,
+  operating_system {
+    type = "win10"
+  }
+
+  cpu {
+    cores   = 2
+    sockets = 1
+  }
+
+  memory {
+    dedicated = 3096
+  }
+
+  clone {
+    vm_id = var.vm_id_winsrv2019
+    full  = var.pm_full_clone
+  }
+
+  agent {
+    # read 'Qemu guest agent' section, change to true only when ready
+    enabled = true
+  }
+
+  network_device {
+    bridge  = "vmbr3"
+    model   = "e1000"
+    vlan_id = 10
+  }
+
+  initialization {
+    datastore_id = var.storage
+    dns {
+      servers = [
+        "192.168.10.1",
+        "1.1.1.1",
       ]
     }
-   nameserver = "192.168.10.1"
-   ipconfig0 = "ip=192.168.10.11/24,gw=192.168.10.1"
+    ip_config {
+      ipv4 {
+        address = "192.168.10.11/24"
+        gateway = "192.168.10.1"
+      }
+    }
+  }
 }
 
-resource "proxmox_vm_qemu" "DC03" {
-    name = "GOAD-DC03"
-    desc = "DC03 - windows server 2016 - 192.168.10.12"
-    qemu_os = "win10"
+resource "proxmox_virtual_environment_vm" "dc03" {
+  name        = "GOAD-DC03"
+  description = "DC03 - windows server 2016 - 192.168.10.12"
+  node_name   = var.pm_node
+  pool_id     = var.pm_pool
 
-    target_node = var.pm_node
-    pool = var.pm_pool
+  operating_system {
+    type = "win10"
+  }
 
+  cpu {
+    cores   = 2
     sockets = 1
-    cores = 2
-    memory = 3096
-    agent = 1
-    clone = "WinServer2016x64-cloudinit-qcow2"
-    full_clone = var.pm_full_clone
+  }
 
-    network {
-      bridge    = "vmbr3"
-      model     = "e1000"
-      tag       = 10 
-    }
-    lifecycle {
-      ignore_changes = [
-        disk,
+  memory {
+    dedicated = 3096
+  }
+
+  clone {
+    vm_id = var.vm_id_winsrv2016
+    full  = var.pm_full_clone
+  }
+
+  agent {
+    # read 'Qemu guest agent' section, change to true only when ready
+    enabled = true
+  }
+
+  network_device {
+    bridge  = "vmbr3"
+    model   = "e1000"
+    vlan_id = 10
+  }
+
+  initialization {
+    datastore_id = var.storage
+    dns {
+      servers = [
+        "192.168.10.1",
+        "1.1.1.1",
       ]
     }
-   nameserver = "192.168.10.1"
-   ipconfig0 = "ip=192.168.10.12/24,gw=192.168.10.1"
+    ip_config {
+      ipv4 {
+        address = "192.168.10.12/24"
+        gateway = "192.168.10.1"
+      }
+    }
+  }
 }
 
-resource "proxmox_vm_qemu" "srv02" {
-    name = "GOAD-SRV02"
-    desc = "SRV02 - windows server 2019 - 192.168.10.22"
-    qemu_os = "win10"
-    target_node = var.pm_node
-    pool = var.pm_pool
-    sockets = 1
-    cores = 2
-    memory = 4096
-    agent = 1
-    clone = "WinServer2019x64-cloudinit-qcow2"
-    full_clone = var.pm_full_clone
+resource "proxmox_virtual_environment_vm" "srv02" {
+  name        = "GOAD-SRV02"
+  description = "SRV02 - windows server 2019 - 192.168.10.22"
+  node_name   = var.pm_node
+  pool_id     = var.pm_pool
 
-    network {
-      bridge    = "vmbr3"
-      model     = "e1000"
-      tag       = 10
-    }
-    lifecycle {
-      ignore_changes = [
-        disk,
+  operating_system {
+    type = "win10"
+  }
+
+  cpu {
+    cores   = 2
+    sockets = 1
+  }
+
+  memory {
+    dedicated = 4096
+  }
+
+  clone {
+    vm_id = var.vm_id_winsrv2019
+    full  = var.pm_full_clone
+  }
+
+  agent {
+    # read 'Qemu guest agent' section, change to true only when ready
+    enabled = true
+  }
+
+  network_device {
+    bridge  = "vmbr3"
+    model   = "e1000"
+    vlan_id = 10
+  }
+
+  initialization {
+    datastore_id = var.storage
+    dns {
+      servers = [
+        "192.168.10.1",
+        "1.1.1.1",
       ]
     }
-    nameserver = "192.168.10.1"
-    ipconfig0 = "ip=192.168.10.22/24,gw=192.168.10.1"
+    ip_config {
+      ipv4 {
+        address = "192.168.10.22/24"
+        gateway = "192.168.10.1"
+      }
+    }
+  }
 }
 
-resource "proxmox_vm_qemu" "srv03" {
-    name = "GOAD-SRV03"
-    desc = "SRV03 - windows server 2016 - 192.168.10.23"
-    qemu_os = "win10"
-    target_node = var.pm_node
-    pool = var.pm_pool
-    sockets = 1
-    cores = 2
-    memory = 4096
-    agent = 1
-    clone = "WinServer2016x64-cloudinit-qcow2"
-    full_clone = var.pm_full_clone
+resource "proxmox_virtual_environment_vm" "srv03" {
+  name        = "GOAD-SRV03"
+  description = "SRV03 - windows server 2016 - 192.168.10.23"
+  node_name   = var.pm_node
+  pool_id     = var.pm_pool
 
-    network {
-      bridge    = "vmbr3"
-      model     = "e1000"
-      tag       = 10
-    }
-    
-    lifecycle {
-      ignore_changes = [
-        disk,
+  operating_system {
+    type = "win10"
+  }
+
+  cpu {
+    cores   = 2
+    sockets = 1
+  }
+
+  memory {
+    dedicated = 4096
+  }
+
+  clone {
+    vm_id = var.vm_id_winsrv2016
+    full  = var.pm_full_clone
+  }
+
+  agent {
+    # read 'Qemu guest agent' section, change to true only when ready
+    enabled = true
+  }
+
+  network_device {
+    bridge  = "vmbr3"
+    model   = "e1000"
+    vlan_id = 10
+  }
+
+  initialization {
+    datastore_id = var.storage
+    dns {
+      servers = [
+        "192.168.10.1",
+        "1.1.1.1",
       ]
     }
-    nameserver = "192.168.10.1"
-    ipconfig0 = "ip=192.168.10.23/24,gw=192.168.10.1"
+    ip_config {
+      ipv4 {
+        address = "192.168.10.23/24"
+        gateway = "192.168.10.1"
+      }
+    }
+  }
 }
 
+resource "proxmox_virtual_environment_vm" "ws01" {
+  name        = "GOAD-WS01"
+  description = "WS01 - windows 10 - 192.168.10.30"
+  node_name   = var.pm_node
+  pool_id     = var.pm_pool
+
+  operating_system {
+    type = "win10"
+  }
+
+  cpu {
+    cores   = 2
+    sockets = 1
+  }
+
+  memory {
+    dedicated = 4096
+  }
+
+  clone {
+    vm_id = var.vm_id_win10
+    full  = var.pm_full_clone
+  }
+
+  agent {
+    # read 'Qemu guest agent' section, change to true only when ready
+    enabled = true
+  }
+
+  network_device {
+    bridge  = "vmbr3"
+    model   = "e1000"
+    vlan_id = 10
+  }
+
+  initialization {
+    datastore_id = var.storage
+    dns {
+      servers = [
+        "192.168.10.1",
+        "1.1.1.1",
+      ]
+    }
+    ip_config {
+      ipv4 {
+        address = "192.168.10.30/24"
+        gateway = "192.168.10.1"
+      }
+    }
+  }
+}
