419a32e859f56cdf7a2be2c31c0a4f71224bdf50
svetterIO <2234705+svetterIO@users.noreply.github.com>
2024-02-23 16:16:45 +0100

    Feature: GOAD for Windows
    Porting of goad.sh and check.sh to PowerShell variants incl. following new features:
    - Task: purge (destroy VM without confirmation)
    - vagrant tasks: reload, snapshot, resume, suspend, validate
    - optional Kali VM via -k $True/$False/O (only operate on Kali)
    - Enable GUI on Provider: $True/$False or 1/0
    
    Warning:
    Successful provisioning tested only for VMware. VirtualBox: hanging or unreachable VMs. Proxmox: not tested. Providers are kept for testing purposed only.

---
+++ goad.ps1
@@ -0,0 +1,155 @@
+param (
+  [Parameter(Mandatory,HelpMessage="Task to execute: check,install")]
+  [ValidateSet("check","install","status","start","stop","destroy","restart","snapshot","resume","suspend","validate","purge", IgnoreCase = $true)]
+  [Alias('t')]
+  [string]$TASK,
+  [Parameter(Mandatory,HelpMessage="Lab to use: GOAD, GOAD-light, NHA")]
+  [ValidateSet("GOAD","GOAD-light","NHA", IgnoreCase = $true)]
+  [Alias('l')]
+  [string]$LAB,
+  [Parameter(Mandatory,HelpMessage="Provider to use: virtualbox, vmware, azure, proxmox")]
+  [ValidateSet("virtualbox","vmware","azure","proxmox", IgnoreCase = $true)]
+  [Alias('p')]
+  [string]$PROVIDER,
+  [Parameter(Mandatory,HelpMessage="Method to use: podman, docker")]
+  [ValidateSet("podman","docker", IgnoreCase = $true)]
+  [Alias('m')]
+  [string]$METHOD,
+  [Parameter(Mandatory=$false,HelpMessage="Install kali/rolling: `$True/`$False/O (only)")]
+  [ValidateSet($true,$false,"O", IgnoreCase = $true)]
+  [Alias('k')]
+  [string]$KALI,
+  [Parameter(Mandatory=$false,HelpMessage="Enable GUI on Provider: `$True/`$False or 1/0")]
+  [ValidateSet($true,$false, IgnoreCase = $true)]
+  [Alias('g')]
+  [bool]$GUI
+)
+
+# Global variables
+$ERROR = "[!]"
+$OK    = "[x]"
+$ADD   = "[+]"
+$INFO  = "[-]"
+
+# install VMs into provider
+function install_providing{
+  Push-Location
+  try{
+    switch( $PROVIDER )
+    {
+      {("virtualbox") -or ("vmware")} {
+        cd "ad/$LAB/providers/$PROVIDER"
+        $VAGRANT_COMMAND="vagrant"
+        if ($KALI -eq $True){ $VAGRANT_COMMAND+=" --with-optional-boxes" }
+        if ($KALI -eq "O"){ $VAGRANT_COMMAND+=" --only-optional-boxes" }
+        if ($GUI -eq $True){ $VAGRANT_COMMAND+=" --with-gui" }
+        $VAGRANT_COMMAND+=" up"
+        Write-Output "$OK launch vagrant: $VAGRANT_COMMAND"
+        iex($VAGRANT_COMMAND)
+        if ($LASTEXITCODE -ne 0){
+          Write-Output "$ERROR vagrant finish with error abort"
+          exit 1
+        }
+      }
+    }
+  }finally{
+    Pop-Location
+  }
+}
+
+# configure VMs on provider
+function install_provisioning{
+  switch( $PROVIDER )
+  {
+    {("virtualbox") -or ("vmware") -or ("proxmox")} {
+      Write-Output "$INFO successful provisioning tested only for vmware. Virtualbox: hanging VMs. Proxmox: not tested. Providers are kept for testing purposed only."
+      $ALREADY_BUILD=iex("$METHOD images | findstr ""goadansible""")
+      if (-not $ALREADY_BUILD){
+        Write-Output "$ADD Build container: $METHOD build -t goadansible ."
+        $BUILD=iex("$METHOD build -t goadansible .")
+        if ($LASTEXITCODE -ne 0){
+          Write-Output "$ERROR $METHOD build failed"
+          exit 1
+        }
+        Write-Output "$OK Container goadansible creation complete"
+      }
+      Write-Output "$OK Start provisioning from $METHOD"
+      Write-Output "$PSScriptRoot"
+      Write-Output "$METHOD run -ti --rm --network host -h goadansible -v $($PSScriptRoot):/goad -w /goad/ansible goadansible /bin/bash -c ""ANSIBLE_COMMAND='ansible-playbook -i ../ad/$LAB/data/inventory -i ../ad/$LAB/providers/$PROVIDER/inventory' ../scripts/provisionning.sh"""
+      iex("$METHOD run -ti --rm --network host -h goadansible -v $($PSScriptRoot):/goad -w /goad/ansible goadansible /bin/bash -c ""ANSIBLE_COMMAND='ansible-playbook -i ../ad/$LAB/data/inventory -i ../ad/$LAB/providers/$PROVIDER/inventory' ../scripts/provisionning.sh""")
+    }
+  }
+}
+
+# issue vagrant command for VMs on provider
+function vagrant_command($command){
+  switch( $PROVIDER )
+  {
+    {("virtualbox") -or ("vmware")} {
+      Push-Location
+      try{
+        cd "ad/$LAB/providers/$PROVIDER"
+        $VAGRANT_COMMAND="vagrant"
+        if ($KALI -eq $True){ $VAGRANT_COMMAND+=" --with-optional-boxes" }
+        if ($KALI -eq "O"){ $VAGRANT_COMMAND+=" --only-optional-boxes" }
+        if ($GUI -eq $True){ $VAGRANT_COMMAND+=" --with-gui" }
+        $VAGRANT_COMMAND+=" $command"
+        Write-Output "$OK vagrant command: $VAGRANT_COMMAND"
+        iex($VAGRANT_COMMAND)
+      }finally{
+        Pop-Location
+      }
+    }
+  }
+}
+
+#Check-function
+function check {
+  Write-Output "$OK Launch check : ./scripts/check.ps1 $PROVIDER $METHOD"
+  & $PSScriptRoot\scripts\check.ps1 $PROVIDER $METHOD 
+  if ($LASTEXITCODE -eq 0){
+    Write-Output "$OK Check is ok, you can start the installation"
+  } else {
+    Write-Output "$ERROR Check is not ok, please fix the errors and retry"
+    Write-Output "$INFO You could also run the setup script"
+  } 
+}
+
+#Install-function
+function install {
+  Write-Output "$OK Launch installation for: $LAB / $PROVIDER / $METHOD"
+  install_providing
+  install_provisioning
+}
+
+#Main-function
+function main {
+  # Remember current directory
+  Push-Location
+  
+  # switch to script directory
+  cd $PSScriptRoot
+
+  switch( $TASK )
+  {
+    check { check }
+    install { install }
+    status { vagrant_command("status") }
+    start { vagrant_command("up") }
+    stop { vagrant_command("halt") }
+    destroy { vagrant_command("destroy") }
+    purge { vagrant_command("destroy -f") }
+    restart { vagrant_command("reload") }
+    snapshot { vagrant_command("snapshot") }
+    resume { vagrant_command("resume") }
+    suspend { vagrant_command("suspend") }
+    validate { vagrant_command("validate") }
+    default { Write-Output "unknow option for TASK" } # NOT required as param([ValidateSet()] takes care
+  }
+
+  # Return to the previous directory 
+  Pop-Location
+}
+
+#Entry point
+main
