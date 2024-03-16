2ad28d76c22617ed350f4c573fdbfc2748b866a6
Mayfly277 <mayfly277@gmail.com>
2024-03-16 22:32:14 +0100

    fix powershell sccm config modules automation issues

--- ansible/roles/sccm/config/boundary/library/sccm_boundary.ps1
+++ ansible/roles/sccm/config/boundary/library/sccm_boundary.ps1
@@ -11,6 +11,7 @@
 #   state: "present" (absent/present)
 
 $ErrorActionPreference = "Stop"
+$ProgressPreference = "SilentlyContinue"
 
 $params = Parse-Args -arguments $args -supports_check_mode $true
 $check_mode = Get-AnsibleParam -obj $params -name "_ansible_check_mode" -type "bool" -default $false
@@ -18,18 +19,18 @@ $check_mode = Get-AnsibleParam -obj $params -name "_ansible_check_mode" -type "b
 $name = Get-AnsibleParam -obj $params -name "name" -type "str" -failifempty $true
 $boundary_type = Get-AnsibleParam -obj $params -name "type" -type "str" -failifempty $true -validateset "IPSubNet","ADSite" ,"IPv6Prefix","IPRange","VPN"
 $boundary_value = Get-AnsibleParam -obj $params -name "value" -type "str" -failifempty $true
-$siteCode = Get-AnsibleParam -obj $params -name "site_code" -type "str"
+$siteCode = Get-AnsibleParam -obj $params -name "site_code" -type "str" -failifempty $true
 $state = Get-AnsibleParam -obj $params -name "state" -type "str" -default "present" -validateset "absent", "present"
+$server = Get-AnsibleParam -obj $params -name "server" -type "str" -failifempty $true
 
 $result = @{
     changed = $false
 }
 
 Import-Module $env:SMS_ADMIN_UI_PATH.Replace("\bin\i386","\bin\configurationmanager.psd1") -force
-
-if ($null -eq $siteCode) {
-    $sc = Get-PSDrive -PSProvider CMSITE
-    $siteCode = $sc.name
+$sc = Get-PSDrive -PSProvider CMSITE
+if ($null -eq $sc) {
+    New-PSDrive -Name $siteCode -PSProvider "CMSite" -Root $server -Description "primary site"
 }
 Set-Location ($siteCode +":")
 
--- ansible/roles/sccm/config/boundary/library/sccm_boundary_group.ps1
+++ ansible/roles/sccm/config/boundary/library/sccm_boundary_group.ps1
@@ -10,13 +10,14 @@
 #   state: "present" (absent/present)
 
 $ErrorActionPreference = "Stop"
+$ProgressPreference = "SilentlyContinue"
 
 $params = Parse-Args -arguments $args -supports_check_mode $true
 $check_mode = Get-AnsibleParam -obj $params -name "_ansible_check_mode" -type "bool" -default $false
 
 $name = Get-AnsibleParam -obj $params -name "name" -type "str" -failifempty $true
 $server = Get-AnsibleParam -obj $params -name "server" -type "str" -failifempty $true
-$siteCode = Get-AnsibleParam -obj $params -name "site_code" -type "str"
+$siteCode = Get-AnsibleParam -obj $params -name "site_code" -type "str" -failifempty $true
 $state = Get-AnsibleParam -obj $params -name "state" -type "str" -default "present" -validateset "absent", "present"
 
 $result = @{
@@ -24,10 +25,9 @@ $result = @{
 }
 
 Import-Module $env:SMS_ADMIN_UI_PATH.Replace("\bin\i386","\bin\configurationmanager.psd1") -force
-
-if ($null -eq $siteCode) {
-    $sc = Get-PSDrive -PSProvider CMSITE
-    $siteCode = $sc.name
+$sc = Get-PSDrive -PSProvider CMSITE
+if ($null -eq $sc) {
+    New-PSDrive -Name $siteCode -PSProvider "CMSite" -Root $server -Description "primary site"
 }
 Set-Location ($siteCode +":")
 
