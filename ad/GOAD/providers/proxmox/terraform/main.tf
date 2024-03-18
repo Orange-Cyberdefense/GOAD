f4831126a0b3dd7fc8bfef1b33635f324cfa79ca
Aan <6284204+aancw@users.noreply.github.com>
2024-03-19 00:30:32 +0700

    Change proxmox provider in terraform to support proxmox v8.x

--- ad/GOAD/providers/proxmox/terraform/main.tf
+++ ad/GOAD/providers/proxmox/terraform/main.tf
@@ -1,8 +1,12 @@
 terraform {
   required_providers {
+    #proxmox = {
+      #source = "telmate/proxmox"
+      #version = ">=1.0.0"
+    #}
     proxmox = {
-      source = "telmate/proxmox"
-      version = ">=1.0.0"
+      source  = "thegameprofi/proxmox"
+      version = ">= 2.9.15"
     }
   }
 }
