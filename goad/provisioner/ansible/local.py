c6d9a8504d8643e45190fa32cda2d2eb1eb4550e
Mayfly277 <mayfly277@gmail.com>
2024-08-28 13:09:16 +0200

    fix playbook args

--- goad/provisioner/ansible/local.py
+++ goad/provisioner/ansible/local.py
@@ -14,7 +14,7 @@ class LocalAnsibleProvisionerCmd(Ansible):
 
         Log.info(f'Run playbook : {playbook} with inventory file(s) : {", ".join(inventories)}')
 
-        args = f'-i {"-i ".join(inventories)} {playbook}'
+        args = f'-i {" -i ".join(inventories)} {playbook}'
 
         run_complete = False
         nb_try = 0
