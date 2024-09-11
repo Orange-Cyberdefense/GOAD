101d3b71a2331982520d161e844139b3e038457b
Mayfly277 <mayfly277@gmail.com>
2024-09-11 12:49:48 +0200

    update nha providers

--- ad/NHA/providers/aws/windows.tf
+++ ad/NHA/providers/aws/windows.tf
@@ -1,40 +1,48 @@
+# t2.medium = 2cpu / 4GB
+# t2.large  = 2cpu / 8GB
+# t2.xlarge = 4cpu / 16GB
 "dc01" = {
   name               = "dc01"
+  domain             = "ninja.hack"
   windows_sku        = "2019-Datacenter"
-  windows_version    = "latest"
+  ami                = "ami-018ebfbd6b0a4c605"
+  instance_type      = "t2.medium"
   private_ip_address = "{{ip_range}}.10"
   password           = "8dCT-6546541qsdDJjgScp"
-  size               = "Standard_B2s"
 }
 "dc02" = {
   name               = "dc02"
+  domain             = "academy.ninja.lan"
   windows_sku        = "2019-Datacenter"
-  windows_version    = "latest"
+  ami                = "ami-018ebfbd6b0a4c605"
+  instance_type      = "t2.medium"
   private_ip_address = "{{ip_range}}.20"
   password           = "Ufe-qsdaz789bVXSx9rk"
-  size               = "Standard_B2s"
 }
 "srv01" = {
   name               = "srv01"
+  domain             = "academy.ninja.lan"
   windows_sku        = "2019-Datacenter"
-  windows_version    = "latest"
+  ami                = "ami-018ebfbd6b0a4c605"
+  instance_type      = "t2.medium"
   private_ip_address = "{{ip_range}}.21"
   password           = "EaqsdP+xh7sdfzaRk6j90"
-  size               = "Standard_B2s"
 }
 "srv02" = {
   name               = "srv02"
+  domain             = "academy.ninja.lan"
   windows_sku        = "2019-Datacenter"
-  windows_version    = "latest"
+  ami                = "ami-018ebfbd6b0a4c605"
+  instance_type      = "t2.medium"
   private_ip_address = "{{ip_range}}.22"
   password           = "978i2pF43UqsdqsdJ-qsd"
-  size               = "Standard_B2s"
 }
 "srv03" = {
   name               = "srv03"
+  domain             = "academy.ninja.lan"
   windows_sku        = "2019-Datacenter"
-  windows_version    = "latest"
+  ami                = "ami-018ebfbd6b0a4c605"
+  instance_type      = "t2.medium"
   private_ip_address = "{{ip_range}}.23"
   password           = "EalwxkfhqsdP+xh7sdfzaRk6j90"
-  size               = "Standard_B2s"
 }
