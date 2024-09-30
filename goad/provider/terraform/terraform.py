a632cf1888912d3fe5469e14d64f83b61b45d2e9
Mayfly277 <mayfly277@gmail.com>
2024-10-01 00:52:08 +0200

    remove unused function

--- goad/provider/terraform/terraform.py
+++ goad/provider/terraform/terraform.py
@@ -40,18 +40,3 @@ class TerraformProvider(Provider):
 
     def ssh_jumpbox(self):
         pass
-
-    def install_extension(self, extension):
-        super().install_extension(extension)
-        extension_name = extension.name
-        extension_filename = extension_name + '.tf'
-        provider_extension_file = GoadPath.get_extension_provider_path(extension_name, self.provider_name) + os.path.sep + extension_filename
-        if os.path.isfile(provider_extension_file):
-            Log.success(f'Found provider extension file : {provider_extension_file}')
-            destination_file = GoadPath.get_lab_providers_path(self.lab_name) + os.path.sep + self.provider_name + os.path.sep + 'terraform' + os.path.sep + extension_filename
-            shutil.copy(provider_extension_file, destination_file)
-            Log.success(f'Extension file {extension_filename} copied')
-            Log.info('relaunch providing for the extension')
-            self.install()
-        else:
-            Log.error(f'Extension file {extension_filename} not found')
