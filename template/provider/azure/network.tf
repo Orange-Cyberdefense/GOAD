8c8a557a85c54161852e7a16287506acb602d6da
Mat Ruane <mat.ruane@zxsecurity.co.nz>
2025-01-08 09:18:52 +1300

    Add NSG to allow only SSH inbound

--- template/provider/azure/network.tf
+++ template/provider/azure/network.tf
@@ -12,6 +12,29 @@ resource "azurerm_subnet" "subnet" {
   address_prefixes     = ["{{ip_range}}.0/24"]
 }
 
+resource "azurerm_network_security_group" "nsg" {
+  name                 = "{{lab_name}}-subnet-nsg"
+  location             = azurerm_resource_group.resource_group.location
+  resource_group_name  = azurerm_resource_group.resource_group.name
+
+  security_rule {
+    name                          = "AllowSSHInboundOnly"
+    priority                      = 100
+    direction                     = "Inbound"
+    access                        = "Allow"
+    protocol                      = "Tcp"
+    source_port_range             = "*"
+    destination_port_range        = "22"
+    source_address_prefix         = "*"
+    destination_address_prefix    = "*"
+  }
+}
+
+resource "azurerm_subnet_network_security_group_association" "nsg_association" {
+  subnet_id                       = azurerm_subnet.subnet.id
+  network_security_group_id       = azurerm_network_security_group.nsg.id
+}
+
 resource "azurerm_network_interface" "goad-vm-nic" {
   for_each = var.vm_config
 
