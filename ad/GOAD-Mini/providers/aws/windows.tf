8fe1f8de06488ad331c7df1a04e249394b617cb5
Danus <117581137+CymDanus@users.noreply.github.com>
2025-03-26 15:38:05 +0200

    Update windows.tf
    ami-001303fabe63b6763 This is the new AMI ID, the other version does not exist and causes errors

--- ad/GOAD-Mini/providers/aws/windows.tf
+++ ad/GOAD-Mini/providers/aws/windows.tf
@@ -2,7 +2,7 @@
   name               = "dc01"
   domain             = "sevenkingdoms.local"
   windows_sku        = "2019-Datacenter"
-  ami                = "ami-0f86e4f2f0ee6d61f"
+  ami                = "ami-001303fabe63b6763"
   instance_type      = "t2.medium"
   private_ip_address = "{{ip_range}}.10"
   password           = "8dCT-DJjgScp"
