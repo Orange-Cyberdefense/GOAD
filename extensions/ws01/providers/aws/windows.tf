1147c9eb493e710fe922c6deda8a4f88189acb66
Samir Benzammour <samir.benzammour@gmail.com>
2025-12-08 14:32:11 +0100

    chore(AWS): Update Window AMIs
    AWS updates AMIs from time to time, which need to be updated in the Terraform configuration

--- extensions/ws01/providers/aws/windows.tf
+++ extensions/ws01/providers/aws/windows.tf
@@ -3,7 +3,7 @@
   name               = "ws01"
   domain             = "sevenkingdoms.local"
   windows_sku        = "2019-Datacenter"
-  ami                = "ami-03440f0d88fea1060"
+  ami                = "ami-0a4c1700182f3bc09"
   instance_type      = "t2.medium"
   private_ip_address = "{{ip_range}}.31"
   password           = "EP+xh7Rk6j90"
