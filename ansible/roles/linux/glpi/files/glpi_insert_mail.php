47895b20a1880d5f1030acddbfaa3a4112fcdc1b
Mayfly277 <mayfly277@gmail.com>
2026-03-02 15:02:34 +0100

    add email glpi

---
+++ ansible/roles/linux/glpi/files/glpi_insert_mail.php
@@ -0,0 +1,43 @@
+<?php
+// usage: php glpi_password.php db_host db_name db_user db_password
+
+if ($argc !== 7) {
+    echo "Usage: php {$argv[0]} db_host db_name db_user db_password\n";
+    exit(1);
+}
+
+$db_host     = $argv[1];
+$db_name     = $argv[2];
+$db_user     = $argv[3];
+$db_password = $argv[4];
+
+$mysqli = new mysqli($db_host, $db_user, $db_password, $db_name);
+if ($mysqli->connect_error) {
+    die("Connection failed: " . $mysqli->connect_error . "\n");
+}
+$mysqli->set_charset('utf8mb4');
+
+// Mise à jour du mot de passe + timestamp DATETIME
+$sql = "
+    INSERT INTO glpi_useremails (users_id, email, is_default)
+    VALUES (
+        (SELECT id FROM glpi_users WHERE name = 'glpi'),
+        'glpi@dracarys.lab',
+        1
+    );
+";
+
+$stmt = $mysqli->prepare($sql);
+
+if ($stmt->execute()) {
+    if ($stmt->affected_rows > 0) {
+        echo "Email setup successfully'\n";
+    } else {
+        echo "No user updated.\n";
+    }
+} else {
+    echo "SQL error: " . $stmt->error . "\n";
+}
+
+$stmt->close();
+$mysqli->close();
